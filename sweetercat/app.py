e69daf4971d2e1034250c5d14dd5a07cdafb87e0
Jason Neal <jason.neal@astro.up.pt>
2017-07-19 10:11:48 +0100

    Adding histograms to the axis.
    Only placed, no selection effect.

--- sweetercat/app.py
+++ sweetercat/app.py
@@ -7,9 +7,10 @@ from PyAstronomy import pyasl
 from bokeh.embed import components
 from bokeh.resources import INLINE
 from bokeh.palettes import Viridis11
+from bokeh.layouts import row, column
 from bokeh.util.string import encode_utf8
-from bokeh.plotting import figure, ColumnDataSource
-from bokeh.models import HoverTool, ColorBar, LinearColorMapper, LabelSet
+from bokeh.plotting import figure, ColumnDataSource, curdoc
+from bokeh.models import HoverTool, ColorBar, LinearColorMapper, LabelSet, Spacer
 
 COLORS = Viridis11
 
@@ -242,10 +243,69 @@ def plot_page(df, columns, request, page):
         fig.xaxis.axis_label = x.name
         fig.yaxis.axis_label = y.name
 
+    # Horizontal historgram
+    print(x[~np.isfinite(x)])
+    hhist, hedges = np.histogram(x, bins=20)
+    hzeros = np.zeros(len(hedges) - 1)
+    hmax = max(hhist) * 1.1
+
+    LINE_ARGS = dict(color="#3A5785", line_color=None)
+
+    ph = figure(toolbar_location=None, plot_width=fig.plot_width, plot_height=200, x_range=fig.x_range,
+                y_range=(-hmax, hmax), min_border=10, min_border_left=50, y_axis_location="right")
+    ph.xgrid.grid_line_color = None
+    ph.yaxis.major_label_orientation = np.pi / 4
+    ph.background_fill_color = "#fafafa"
+
+    ph.quad(bottom=0, left=hedges[:-1], right=hedges[1:], top=hhist, color="white", line_color="#3A5785")
+    hh1 = ph.quad(bottom=0, left=hedges[:-1], right=hedges[1:], top=hzeros, alpha=0.5, **LINE_ARGS)
+    hh2 = ph.quad(bottom=0, left=hedges[:-1], right=hedges[1:], top=hzeros, alpha=0.1, **LINE_ARGS)
+
+    # vertical historgram
+    vhist, vedges = np.histogram(y, bins=20)
+    vzeros = np.zeros(len(vedges) - 1)
+    vmax = max(vhist) * 1.1
+
+    pv = figure(toolbar_location=None, plot_width=200, plot_height=fig.plot_height, x_range=(-vmax, vmax),
+                y_range=fig.y_range, min_border=10, y_axis_location="right")
+    pv.ygrid.grid_line_color = None
+    pv.xaxis.major_label_orientation = np.pi / 4
+    pv.background_fill_color = "#fafafa"
+
+    pv.quad(left=0, bottom=vedges[:-1], top=vedges[1:], right=vhist, color="white", line_color="#3A5785")
+    # vh1 = pv.quad(left=0, bottom=vedges[:-1], top=vedges[1:], right=vzeros, alpha=0.5, **LINE_ARGS)
+    # vh2 = pv.quad(left=0, bottom=vedges[:-1], top=vedges[1:], right=vzeros, alpha=0.1, **LINE_ARGS)
+
+    # Selection updating code - not working.
+    # def update(attr, old, new):
+    #     inds = np.array(new['1d']['indices'])
+    #     if len(inds) == 0 or len(inds) == len(x):
+    #         hhist1, hhist2 = hzeros, hzeros
+    #         vhist1, vhist2 = vzeros, vzeros
+    #     else:
+    #         neg_inds = np.ones_like(x, dtype=np.bool)
+    #         neg_inds[inds] = False
+    #         hhist1, _ = np.histogram(x[inds], bins=hedges)
+    #         vhist1, _ = np.histogram(y[inds], bins=vedges)
+    #         hhist2, _ = np.histogram(x[neg_inds], bins=hedges)
+    #         vhist2, _ = np.histogram(y[neg_inds], bins=vedges)
+    #
+    #     hh1.data_source.data["top"]   =  hhist1
+    #     hh2.data_source.data["top"]   = -hhist2
+    #     vh1.data_source.data["right"] =  vhist1
+    #     vh2.data_source.data["right"] = -vhist2
+    #
+    # cr.data_source.on_change('selected', update)
+    #
+    # curdoc().add_root(layout)
+    # curdoc().title = "Selection Histogram"
+
+    layout = column(row(fig, pv), row(ph, Spacer(width=200, height=200)))
+
     js_resources = INLINE.render_js()
     css_resources = INLINE.render_css()
 
-    script, div = components(fig)
+    script, div = components(layout)
     html = render_template(
         'plot.html',
         plot_script=script,
