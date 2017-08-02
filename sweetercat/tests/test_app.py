43c9e4c4844d19ed659ddf0092b793971fc1a9b5
Jason Neal <jason.neal@astro.up.pt>
2017-08-02 23:56:37 +0100

    Remove test_status_codes as built into other tests now.
    Tidy up some other tests.

--- sweetercat/tests/test_app.py
+++ sweetercat/tests/test_app.py
@@ -19,6 +19,7 @@ def test_homepage(client):
     homepage = client.get(url_for("homepage"))
     assert homepage.status_code == 200
     assert b"A detailed description of each field can be found" in homepage.data
+
     # Link to SwEEt-Cat
     assert b'<a href="https://www.astro.up.pt/resources/sweet-cat/">SWEET-Cat</a>' in homepage.data
 
@@ -31,12 +32,6 @@ def test_parameter_description_on_homepage(client):
     table = client.get("/static/table.pdf")
     assert table.status_code == 200
     assert b"<dc:format>application/pdf</dc:format>\n" in table.data
-
-
-def test_status_codes(client):
-    """Test that all pages return status code: 200 using the end_points"""
-    for end_point in ('publications',):
-        assert client.get(url_for(end_point)).status_code == 200
 
 
 # Need to check for 'stardetail' which also requires a star name.
@@ -71,9 +66,9 @@ def test_request_paths():
 
 def test_publication_headings(client):
     """ Test for the labels Abstact:, Authors: etc."""
-    response = client.get(url_for("publications"))
+    publications = client.get(url_for("publications"))
     for heading in [b"Main papers", b"Derived papers", b"Authors:", b"Abstract:", b"read more"]:
-        assert heading in response.data
+        assert heading in publications.data
 
 
 def test_publication_response_data(client):
@@ -81,7 +76,9 @@ def test_publication_response_data(client):
 
     Test that links are inserted for the title and "read more" sections.
     """
-    response = client.get(url_for("publications"))
+    publications = client.get(url_for("publications"))
+    assert publications.status_code == 200
+
     with open('data/publications.json') as pubs:
         pubs = json.load(pubs)
     for paper_key in pubs:
@@ -92,12 +89,12 @@ def test_publication_response_data(client):
                     url = paper["adsabs"].replace("&", "&amp;")
                     read_more = '...<a href="{0}" target="_blank"> read more</a>'.format(url).encode('utf-8')
                     title_link = '<a href="{0}" target="_blank">{1}</a>'.format(url, paper["title"]).encode('utf-8')
-                    assert title_link in response.data
-                    assert read_more in response.data
+                    assert title_link in publications.data
+                    assert read_more in publications.data
                 elif "abstract" in key:
-                    assert value[:480] in response.data
+                    assert value[:480] in publications.data
                 else:
-                    assert value in response.data
+                    assert value in publications.data
 
 
 def test_stardetail_template_text(client):
@@ -108,30 +105,37 @@ def test_stardetail_template_text(client):
     sttext = ["General info", "Reference article:", "Right ascension:", "Declination:",
                 "Magnitude:", "Parallax:", "mas", "Atmospheric parameters", "Teff:", "K",
                 "logg:", "[Fe/H]:", "dex", "vt:", "km/s", "Other info", "Mass:"]
-    pltext = ["Planetary information", "Mass", "MJup", "Radius", "RJup", "Density",
-                 "Orbital parameters", "Period:", "days", "Semi-major axis:", "AU",
-                 "Inner habitable zone limit:", "Density", "Outer habitable zone limit:"]
+    # pltext = ["Planetary information", "Mass", "MJup", "Radius", "RJup", "Density",
+    #              "Orbital parameters", "Period:", "days", "Semi-major axis:", "AU",
+    #              "Inner habitable zone limit:", "Density", "Outer habitable zone limit:"]
     df, __ = readSC(nrows=10)
     stars = df.Star.values
+
     for i, star in enumerate(stars):
-        response = client.get(url_for("stardetail", star=star))
+        star_detail = client.get(url_for("stardetail", star=star))
+
         if df["flag"][i]:
-            assert b"Parameters analysed by the Porto group" in response.data
+            assert b"Parameters analysed by the Porto group" in star_detail.data
         else:
-            assert b"Parameters from the literature" in response.data
+            assert b"Parameters from the literature" in star_detail.data
         for text in sttext:
-            assert text.encode("utf-8") in response.data
+            assert text.encode("utf-8") in star_detail.data
 
     # if (star has planet parameters): # Need planetandStar instead of readSC
     # for text in pltext:
-    #     assert text.encode("utf-8") in response.data
+    #     assert text.encode("utf-8") in star_detail.data
 
 
 def test_download_status_code(client):
+    # Download database on-hand
+    assert os.path.isfile('data/sweet-cat.tsv')
+    assert client.get(url_for('download', fname='sweet-cat.tsv')).status_code == 200
+
+    # Download with format conversion
     for fmt in ('csv', 'hdf'):
         fname = 'sweet-cat.{}'.format(fmt)
         assert client.get(url_for('download', fname=fname)).status_code == 200
         assert not os.path.isfile('data/{}'.format(fname))
-    assert client.get(url_for('download', fname='sweet-cat.tsv')).status_code == 200
-    assert os.path.isfile('data/sweet-cat.tsv')
+
+    # Invlaid format
     assert client.get(url_for('download', fname='sweet-cat.fits')).status_code == 302
