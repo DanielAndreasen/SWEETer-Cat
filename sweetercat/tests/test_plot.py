394ceb64c790dca3c2f54e976ca531bc32b86827
Jason Neal <jason.neal@astro.up.pt>
2017-08-05 23:41:57 +0100

    Add fixture with default plotting form data.

--- sweetercat/tests/test_plot.py
+++ sweetercat/tests/test_plot.py
@@ -4,6 +4,15 @@ import pytest
 from flask import url_for
 
 
+@pytest.fixture()
+def form_data():
+    """Default form values for testing."""
+    form = {'color': 'Blue', 'x': 'teff', 'y': 'mass', 'z': 'Vmag',
+            'x1': "None", 'x2': "None", 'y1': "None", 'y2': "None",
+            'xscale': 'linear', 'yscale': 'log', 'checkboxes': ''}
+    return form
+
+
 def test_plot_get_requests(client):
     """Test that all pages return status code: 200 using the end_points"""
     for end_point in ('plot', 'plot_exo'):
@@ -13,57 +22,44 @@ def test_plot_get_requests(client):
         assert b"Select your settings:" in plot.data
 
 
-def test_plot_post_request(client):
-    test_data = {'color': 'Blue', 'x': 'teff', 'y': 'mass', 'z': 'Vmag',
-                 'x1': 8000, 'x2': 2500, 'y1': 0, 'y2': 5,
-                 'xscale': 'linear', 'yscale': 'log', 'checkboxes': ''}
-
+def test_plot_post_request(client, form_data):
     for end_point in ('plot',):  # 'plot_exo'
-        plot = client.post(url_for(end_point), data=test_data, follow_redirects=True)
+        plot = client.post(url_for(end_point), data=form_data, follow_redirects=True)
 
         assert plot.status_code == 200
         assert b"Select your settings:" in plot.data
 
 
-def test_post_z_none_43(client):
+def test_post_z_none_43(client, form_data):
     """Test that setting z to None does not produce an error."""
-    test_data = {"color": "Blue", "x": "teff", "y": "mass", "z": "None",
-                 "x1": 8000, "x2": 2500, "y1": 0, "y2": 5,
-                 "xscale": "linear", "yscale": "log", "checkboxes": ""}
+    form_data["z"] = "None"
 
     for end_point in ("plot", "plot_exo"):
-        plot = client.post(url_for(end_point), data=test_data, follow_redirects=True)
+        plot = client.post(url_for(end_point), data=form_data, follow_redirects=True)
 
         assert plot.status_code == 200
         assert b"Select your settings:" in plot.data
 
 
-def test_title_and_axis_labels(client):
+def test_title_and_axis_labels(client, form_data):
     for xname, yname in zip(("teff", "vt", "par"), ("mass", "Vabs", "logg")):
-        # test_data = {'color': 'Blue', 'x': xname, 'y': yname, 'z': 'Vmag',
-        #          'x1':  "None", 'x2':  "None", 'y1':  "None", 'y2': "None",
-        #          'xscale': 'linear', 'yscale': 'log', 'checkboxes': ''}
-        print(xname, yname)
-        test_data = {'color': 'Blue', 'x': xname, 'y': yname, 'z': 'Vmag',
-                     'x1': "", 'x2': "", 'y1': "", 'y2': "",
-                     'xscale': 'linear', 'yscale': 'log', 'checkboxes': ''}
-
+        form_data["x"], form_data["y"], form_data["yscale"] = xname, yname, "log"
         title = '"text":"{0} vs. {1}:'.format(xname, yname)
         xlabel = '"axis_label":"{0}"'.format(xname)
         ylabel = '"axis_label":"{0}"'.format(yname)
 
         for end_point in ('plot',):  # 'plot_exo'
-            plot = client.post(url_for(end_point), data=test_data, follow_redirects=True)
-            print(plot)
+            plot = client.post(url_for(end_point), data=form_data, follow_redirects=True)
+
             for feature in [title, xlabel, ylabel]:
                 assert feature.encode("utf-8") in plot.data
 
 
-def test_feh_with_log_scale(client):
-    test_data = {'color': 'Blue', 'x': "feh", 'y': "feh", 'z': 'Vmag',
-                 'x1': "None", 'x2': "None", 'y1': "None", 'y2': "None",
-                 'xscale': 'linear', 'yscale': 'log', 'checkboxes': ''}
+def test_feh_with_log_scale(client, form_data):
+    form_data["x"] = "feh"
+    form_data["xscale"] = "log"
 
-    for end_point in ('plot',):  # 'plot_exo'
-        plot = client.post(url_for(end_point), data=test_data, follow_redirects=True)
-        assert plot.status_code == 200
+    plot = client.post(url_for('plot'), data=form_data, follow_redirects=True)
+    # Just a random test so that the errors are raised at the moment.
+    # Should add an assert for how the correct functionality should work.
+    assert plot.status_code == 200
