40ed093d062183fad1c214f4656e4bd349445b12
DanielAndreasen <daniel.andreasen@astro.up.pt>
2017-08-11 11:27:12 +0100

    Use sample (random) instead of index

--- sweetercat/tests/test_app.py
+++ sweetercat/tests/test_app.py
@@ -30,7 +30,7 @@ def test_stardetail_status_code(client, SCdata, planetStardata):
     """Test stardetail will return status code: 200 when submitted with star"""
     # Passing in planetStardata moves the setup time (~4.8s) of the caching etc into the fixture, which can be shared between tests.
     df, _ = SCdata
-    df = df[:5]
+    df = df.sample(5)
     # All stars are a slow test
     stars = df.Star.values
     for star in stars:
@@ -40,7 +40,7 @@ def test_stardetail_status_code(client, SCdata, planetStardata):
 def test_stardetail_request_path(SCdata):
     """Test that the stardetail renders properly"""
     df, _ = SCdata
-    df = df[:50]
+    df = df.sample(50)
     stars = df.Star.values
     for star in stars:
         # BD+ stars have replaced it with a space. Not a problem in app since
@@ -103,7 +103,7 @@ def test_stardetail_template_text(client, SCdata):
     #           "Orbital parameters", "Period:", "days", "Semi-major axis:", "AU",
     #           "Inner habitable zone limit:", "Density", "Outer habitable zone limit:"]
     df, __ = SCdata
-    df = df[:5]
+    df = df.sample(5)
     stars = df.Star.values
 
     for i, star in enumerate(stars):
