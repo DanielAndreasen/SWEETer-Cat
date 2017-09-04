693b3b5594d52af4254695d1310522f11f50306c
DanielAndreasen <daniel.andreasen@astro.up.pt>
2017-09-04 15:04:06 +0100

    Dynamically use the PORT set by heroku

--- sweetercat/app.py
+++ sweetercat/app.py
@@ -119,4 +119,5 @@ def download(fname):
 
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port, debug=True)
