79c03e990fd1f21e14ab51efac8ed5fc20bde935
Jason Neal <jason.neal@astro.up.pt>
2017-09-24 10:22:15 +0100

    Add publication data fixture.

--- sweetercat/conftest.py
+++ sweetercat/conftest.py
@@ -1,4 +1,5 @@
 """Conftest is for fixtures that will run in all test files."""
+import json
 import pytest
 from app import app as sc_app
 from utils import readSC, planetAndStar
@@ -21,3 +22,11 @@ def SCdata():
 def planetStardata():
     """SWEET-Cat + ExoplanetEU +  database fixture."""
     return planetAndStar()
+
+
+@pytest.fixture()
+def publication_data():
+    """Load publication data file."""
+    with open('data/publications.json') as pubs:
+        pubs = json.load(pubs)
+    return pubs
