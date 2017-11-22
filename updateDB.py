7b848725501b77f0c4c2b347c1db0f52e0faec6a
DanielAndreasen <daniel.andreasen@astro.up.pt>
2017-11-22 14:05:11 +0000

    New update and convert bytes to str

--- updateDB.py
+++ updateDB.py
@@ -1,33 +1,18 @@
 import os
-import shutil
-import warnings
 from PyAstronomy import pyasl
-from astropy.io import votable
 
 from tornado.ioloop import IOLoop
 from apscheduler.schedulers.tornado import TornadoScheduler
 
 
-def convert_to_csv(fname):
-    d = os.path.dirname(fname)
-    with warnings.catch_warnings():
-        warnings.simplefilter("ignore")
-        vot = votable.parse(fname, invalid='mask')
-    vot = vot.get_first_table().to_table(use_names_over_ids=True)
-    df = vot.to_pandas()
-    fout = os.path.join(d, 'exoplanetEU.csv')
-    df.to_csv(fout, index=False)
-
-
 def tick():
     print('Downloading exoplanetEU')
-    _ = pyasl.ExoplanetEU2(forceUpdate=True)
-    home = os.path.expanduser('~')
-    path1 = os.path.join(home, 'PyAData/pyasl/resBased/epeu.vo.gz')
-    path2 = 'sweetercat/data/exoplanetEU.vo.gz'
-    shutil.copyfile(path1, path2)
-    convert_to_csv(path2)
-    os.remove(path2)
+    df = pyasl.ExoplanetEU2(forceUpdate=True)
+    df = df.getAllDataPandas()
+    df['name'] = [s.decode() if isinstance(s, bytes) else s for s in df['name']]
+    df['star_name'] = [s.decode() if isinstance(s, bytes) else s for s in df['star_name']]
+    path = 'sweetercat/data/exoplanetEU.csv'
+    df.to_csv(path, index=False)
     print('Saved as exoplanetEU.csv')
 
 
