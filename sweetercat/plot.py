ff6eb131c2d00f16571f051ab660cd1cb81adda9
DanielAndreasen <daniel.andreasen@astro.up.pt>
2017-12-12 14:27:27 +0000

    Remove unused code

--- sweetercat/plot.py
+++ sweetercat/plot.py
@@ -40,8 +40,6 @@ def detail_plot(df, tlow, thigh):
 
     hz1 = get_default(df['hz1'].values[0], -2, float)
     hz2 = get_default(df['hz2'].values[0], -1, float)
-    M = get_default(df['mass'].values[0], 1, float)
-    logg = get_default(df['logg'].values[0], 4.44, float)
     color = get_default(df['teff'].values[0], 5777, float)
     tlow = get_default(max(2500, tlow), 2500, int)
     thigh = get_default(min(8500, thigh), 8500, int)
